ff422d563facdd67061e365f2a9994da2c335842
Mati-PC\Mati <maciekprokop7@gmail.com>
2018-06-02 13:40:27 +0200

    [2]Changes made: -user is abble to cut blocks, -added event on lose

--- The Stack/Assets/Scripts/TheStack.cs
+++ The Stack/Assets/Scripts/TheStack.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 
 public class TheStack : MonoBehaviour {
 
-    private const float BOUNDS_SIZE = 3.5f; 
+    private const float BOUNDS_SIZE = 3.5f;   //wielkosc kwadratu 
+    private const float STACK_MOVING_SPEED = 5.0f;
+    private const float ERROR_MARGIN = 0.1f;
+
 
     private GameObject[] theStack;
+    private Vector2 stackBounds = new Vector2(BOUNDS_SIZE, BOUNDS_SIZE);
 
-    private int stackIndex;
-    private int scoreCount = 0;
+    private int stackIndex;       //indeks elementu
+    private int scoreCount = 0;   //wynik
+    private int combo = 0;
+
+    private float tileTransition = 0.0f;        //pozcyja
+    private float tileSpeed = 2.5f;    //szybkosc poruszania kwadratu
+    private float secondaryPosition;   //update pozycji kwadratu
+
+    private bool isMovingOnX = true;    //zmiana poruszania kwadratu
+    private bool gameOver = false;
+
+    private Vector3 desiredPosition;
+    private Vector3 lastTilePosition;
 
 	// Use this for initialization
 
-	private void Start () {
+	private void Start (){
         theStack = new GameObject[transform.childCount];
         for (int i = 0; i < transform.childCount; i++){
             theStack[i] = transform.GetChild(i).gameObject;
@@ -24,22 +39,100 @@ public class TheStack : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	private void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            SpawnTile();
-            scoreCount++;
-        }	
+	private void Update (){
+        if (Input.GetMouseButtonDown(0)){
+            if(PlaceTile()){
+                SpawnTile();
+                scoreCount++;
+            }
+            else{
+                EndGame ();
+            }
+        }
+
+        MoveTile();
+        //move the stack
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, STACK_MOVING_SPEED * Time.deltaTime);
 	}
 
+    private void MoveTile(){
+        if (gameOver){
+            return;
+        }
+
+        tileTransition += Time.deltaTime * tileSpeed;
+        if (isMovingOnX){
+            theStack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(tileTransition) * BOUNDS_SIZE, scoreCount, secondaryPosition);
+        }
+        else{
+            theStack[stackIndex].transform.localPosition = new Vector3(secondaryPosition, scoreCount, Mathf.Sin(tileTransition) * BOUNDS_SIZE);
+        }
+    }
+    
+
     private void SpawnTile(){
+        lastTilePosition = theStack[stackIndex].transform.localPosition;
         stackIndex--;
         if (stackIndex < 0){
             stackIndex = transform.childCount - 1;
         }
+        desiredPosition = (Vector3.down) * scoreCount;
         theStack[stackIndex].transform.localPosition = new Vector3(0, scoreCount, 0);
+        theStack[stackIndex].transform.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);    //zmniejszenie kwadratu
     }
 
-    private void PlaceTile(){
-        
+    private bool PlaceTile(){
+        Transform t = theStack[stackIndex].transform;
+
+        if (isMovingOnX){
+            float deltaX = lastTilePosition.x - t.position.x;
+            if(Mathf.Abs (deltaX) > ERROR_MARGIN){
+
+                //cięcie kwadratu
+                combo = 0;
+                stackBounds.x -= Mathf.Abs(deltaX);
+                if(stackBounds.x <= 0)
+                    return false;
+
+                float middle = lastTilePosition.x + t.localPosition.x / 2;
+                t.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+                t.localPosition = new Vector3(middle - (lastTilePosition.x / 2), scoreCount, lastTilePosition.z);
+
+            }
+        }
+        else{
+            float deltaZ = lastTilePosition.z - t.position.z;
+            if (Mathf.Abs(deltaZ) > ERROR_MARGIN)
+            {
+
+                //cięcie kwadratu
+                combo = 0;
+                stackBounds.y -= Mathf.Abs(deltaZ);
+                if (stackBounds.y <= 0)
+                    return false;
+                
+                float middle = lastTilePosition.z + t.localPosition.z / 2;
+                t.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+                t.localPosition = new Vector3(lastTilePosition.x, scoreCount,middle - (lastTilePosition.z / 2));
+
+            }
+        }
+
+
+        if (isMovingOnX){
+            secondaryPosition = t.localPosition.x;
+        }
+        else{
+            secondaryPosition = t.localPosition.z;
+        }
+
+        isMovingOnX = !isMovingOnX;
+        return true;
+    }
+
+    private void EndGame(){
+        Debug.Log("LOSE");
+        gameOver = true;
+        theStack[stackIndex].AddComponent<Rigidbody>();
     }
 }
